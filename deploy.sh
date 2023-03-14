3cc1c84af4d83241a3f5c60a7237bdd81b6bd049
mh-anwar <mohammad.anwar2939@gmail.com>
2023-03-14 15:28:23 -0400

    Deploy script modified

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
+git subtree pull --prefix build git@github.com:mh-anwar/anwar.git build
+
 touch build/CNAME
 echo "anwar.app" >> build/CNAME
 # Copy 404 page to production build
 cp ./404.html build/404.html
 
 cd ./build
-git add .
-git commit -m "Build Updated"
-git push
+git subtree push --prefix build git@github.com:mh-anwar/anwar.git build
